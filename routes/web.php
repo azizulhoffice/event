f034e8fdec9124305866cd65e528c6612198797b
shamrat1 <yshamrat@gmail.com>
2023-09-12 13:03:38 +0600

    logout redirection fix

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return redirect(route('login'));
+    return Auth::check() ? view('welcome') : redirect('/login');
 });
 
 Auth::routes();
