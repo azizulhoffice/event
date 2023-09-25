74f369b763d1530171102dd35f5c929df17eccdd
mt-azizul <a5@marsanetwork.com>
2023-09-25 19:48:45 +0600

    ordinalNumber function added in helper

--- app/helpers.php
+++ app/helpers.php
@@ -29,3 +29,20 @@ function removeTrailingZeros($number)
 
     return $number;
 }
+function numberToOrdinal($number)
+{
+    if ($number % 100 >= 11 && $number % 100 <= 13) {
+        return $number . 'th';
+    } else {
+        switch ($number % 10) {
+            case 1:
+                return $number . 'st';
+            case 2:
+                return $number . 'nd';
+            case 3:
+                return $number . 'rd';
+            default:
+                return $number . 'th';
+        }
+    }
+}
