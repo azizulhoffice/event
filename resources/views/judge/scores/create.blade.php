3a8989e449de29a413403e2010e1efe6ba17c52f
mt-azizul <a5@marsanetwork.com>
2023-09-25 17:08:46 +0600

    toaster showing time decreasing

--- resources/views/judge/scores/create.blade.php
+++ resources/views/judge/scores/create.blade.php
@@ -364,7 +364,7 @@
         "onclick": null,
         "showDuration": "300",
         "hideDuration": "1000",
-        "timeOut": "5000",
+        "timeOut": "800",
         "extendedTimeOut": "1000",
         "showEasing": "swing",
         "hideEasing": "linear",
